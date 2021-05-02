09027c77f8049e52816f177beca416565dd2fca1
Rajesh Mishra <rajesh@studiographene.com>
2021-05-02 22:27:05 +0530

    UI Template added

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressLayouts = require('express-ejs-layouts');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dashboardRouter = require('./routes/dashboard');
+var applicationRouter = require('./routes/applications');
 
 const db = require('./db/models/index');
 
@@ -15,6 +19,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(expressLayouts);
+ 
+// app.get('/', function(req, res) {
+//   var locals = {
+//     title: 'Page Title',
+//     description: 'Page Description',
+//     header: 'Page Header'
+//   };
+//   res.render('the-view', locals);
+// });
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,6 +38,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/users', dashboardRouter);
+app.use('/applications', applicationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
